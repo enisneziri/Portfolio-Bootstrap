0fa0a3e4eba66b3bdf25d75417528e3e061966f0
Enis <74664187+enisneziri@users.noreply.github.com>
2021-11-22 15:26:15 +0100

    Add files via upload

---
+++ StudentBio with bootstrap/portofolio/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600&display=swap" rel="stylesheet">
+    <title>Hello I am back</title>
+</head>
+<body>
+    <h1>testing</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                Hello
+            </div>
+            <div class="col-7">
+                hello
+            </div>
+        </div>
+    </div>
+</body>
+</html>
